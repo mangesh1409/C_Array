6731b8c303a474a7d56124888438df69adb4adf9
Mangesh Prakash Jadhav <42151779+mangesh1409@users.noreply.github.com>
2019-10-10 23:30:12 +0530

    Update Multipleof11.c

--- Multipleof11.c
+++ Multipleof11.c
@@ -1,7 +1,7 @@
 /*	Accept N numbers from user and display all such numbers which are multiple of 11
 	Input: 
-				N: 10
-				Elements: 11  21  88   32   44   77   99  110   120   66
+		N: 10
+		Elements: 11  21  88   32   44   77   99  110   120   66
 	 Output:  11   88   44   77   99   110   66																*/
 	
 #include<stdio.h>
